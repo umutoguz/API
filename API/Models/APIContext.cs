fdaf2a2845862dffee6e5e023427ac9c1a876537
umutoguz <umutoguzbinay@gmail.com>
2015-12-23 01:15:08 +0200

    2.Commit
    Controller ve Model eklendi

---
+++ API/Models/APIContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class APIContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public APIContext() : base("name=APIContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<API.Models.Purchase> Purchases { get; set; }
+
+        public System.Data.Entity.DbSet<API.Models.Product> Products { get; set; }
+    
+    }
+}
---
+++ API/Models/Purchase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        [Required]
+        public int Date { get; set; }
+        // Foreign Key
+        public int UserId { get; set; }
+
+        public int History { get; set; }
+    }
+}
